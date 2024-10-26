45d72c5fd25f5b8be02a6b9974e5316dcdb82731
Tomasz Kiljańczyk <kiljanczyk@deltologic.com>
2024-10-26 23:57:38 +0200

    Update to dependencies, update kotlin to Kotlin 2

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,9 +3,9 @@
  * Copyright (c) 2021 . All rights reserved.
  * Last modified 4/5/21 12:21 AM
  */
-include ':common'
-include ':dataTransfer'
+include(":common")
+include(":dataTransfer")
 
-include ':dataModel'
-include ':app'
+include(":dataModel")
+include(":app")
 rootProject.name = "LyricCast"
